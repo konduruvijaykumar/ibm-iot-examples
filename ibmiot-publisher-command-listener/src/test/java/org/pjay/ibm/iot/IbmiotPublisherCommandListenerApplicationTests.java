f3cc3da5274e3d602b57be64a954c23cc055a3ff
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-06-18 07:36:55 +0530

    Vijay - 2017/06/18 - IBM IoT examples

---
+++ ibmiot-publisher-command-listener/src/test/java/org/pjay/ibm/iot/IbmiotPublisherCommandListenerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.pjay.ibm.iot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IbmiotPublisherCommandListenerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
